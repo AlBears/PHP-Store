21e6ed35ecb3ed5a51d7400405cdfc2eeb253b77
Alexey Sidorovitch <sidorovitch@gmail.com>
2018-01-12 11:38:57 +0300

    subcategory modal

--- resources/views/admin/products/categories.blade.php
+++ resources/views/admin/products/categories.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <div class="category">
+
         <div class="row expanded column">
             <h2>Product Categories</h2>
         </div>
@@ -47,15 +48,33 @@
                                     <td>{{ $category['slug'] }}</td>
                                     <td>{{ $category['added'] }}</td>
                                     <td width="100" class="text-right">
-                                        <span>
-                                            <a data-open="item-{{$category['id']}}"><i class="fa fa-edit"></i></a>
+                                    
+                                        <span data-tooltip aria-haspopup="true" 
+                                                class="has-tip top" data-disable-hover="false" 
+                                                tabindex="1" title="Add Subcategory">
+
+                                            <a data-open="add-subcategory-{{$category['id']}}"><i class="fa fa-plus"></i></a>
+
                                         </span>
-                                        <span style="display: inline-block">
+
+                                        <span data-tooltip aria-haspopup="true" 
+                                                class="has-tip top" data-disable-hover="false" 
+                                                tabindex="1" title="Edit Category">
+
+                                            <a data-open="item-{{$category['id']}}"><i class="fa fa-edit"></i></a>
+
+                                        </span>
+
+                                        <span style="display: inline-block" data-tooltip aria-haspopup="true" 
+                                                class="has-tip top" data-disable-hover="false" 
+                                                tabindex="1" title="Delete Category">
+
                                             <form method="POST" action="/admin/product/categories/{{$category['id']}}/delete"
                                                   class="delete-item">
                                                 <input type="hidden" name="token" value="{{ \App\Classes\CSRFToken::_token() }}">
                                                 <button type="submit"><i class="fa fa-times delete"></i> </button>
                                             </form>
+
                                         </span>
                                         
                                         <!--Edit Category Modal -->
@@ -73,6 +92,30 @@
                                                                id="{{$category['id']}}"
                                                                name="token" data-token="{{ \App\Classes\CSRFToken::_token() }}"
                                                                value="Update">
+                                                    </div>
+                                                </div>
+                                            </form>
+                                            <a href="/admin/product/categories" class="close-button"
+                                               aria-label="Close modal" type="button">
+                                                <span aria-hidden="true">&times;</span>
+                                            </a>
+                                        </div>
+
+                                        <!--Add Subcategory Modal -->
+                                        <div class="reveal" id="add-subcategory-{{$category['id']}}"
+                                             data-reveal data-close-on-click="false" data-close-on-esc="false"
+                                        data-animation-in="scale-in-up">
+                                            <div class="notification callout primary"></div>
+                                            <h2>Add Subcategory</h2>
+                                            <form>
+                                                <div class="input-group">
+                                                    <input type="text" id="subcategory-name-{{$category['id']}}">
+
+                                                    <div>
+                                                        <input type="submit" class="button add-subcategory"
+                                                               id="{{$category['id']}}"
+                                                               name="token" data-token="{{ \App\Classes\CSRFToken::_token() }}"
+                                                               value="Create">
                                                     </div>
                                                 </div>
                                             </form>
