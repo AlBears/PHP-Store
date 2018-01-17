8b161416cfc9c70eade3b3c2725da197a08eea09
Alexey Sidorovitch <sidorovitch@gmail.com>
2018-01-17 10:52:29 +0300

    featured products, styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,20 +12,31 @@
                 <div> <img src="/images/sliders/slide_3.jpg" alt="Acme Store"> </div>
             </div>
         </section>
-
-        <section>
-            <div id="root">
-                @{{ message }}
+    
+        <section class="display-products" id="root">
+            <div class="row medium-up-4">
+                <h2>Featured Products</h2>
+                <div class="small-12 column" v-for="feature in featured">
+                    <a :href="'/product/' + feature.id">
+                        <div class="card" data-equalizer-watch>
+                            <div class="card-section">
+                                <img :src="'/' + feature.image_path" width="100%" height="200">
+                            </div>
+                            <div class="card-section">
+                                <p>
+                                    @{{ stringLimit(feature.name, 18) }}
+                                </p>
+                                <a :href="'/product/' + feature.id" class="button more expanded">
+                                    See More
+                                </a>
+                                <a :href="'/product/' + feature.id" class="button cart expanded">
+                                    $@{{ feature.price }} - Add to cart
+                                </a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
         </section>
     </div>
-
-    <script type="text/javascript">
-        new Vue({
-            el: '#root',
-            data: {
-                message: 'Hello from Vue object'
-            }
-        });
-    </script>
 @stop
