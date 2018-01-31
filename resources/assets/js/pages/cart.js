d31b4faafb75d1247e3fdec9f4f2d4d9c64b45e0
Alexey Sidorovitch <sidorovitch@gmail.com>
2018-01-31 21:37:42 +0300

    view shopping cart

--- resources/assets/js/pages/cart.js
+++ resources/assets/js/pages/cart.js
@@ -11,6 +11,29 @@
                 loading: false,
                 fail: false,
                 message: ''
+            },
+
+            methods: {
+                displayItems: function(){
+                    this.loading = true;
+                    setTimeout(() => {
+                        axios.get('/cart/items').then(function(response){
+                            if(response.data.fail){
+                                app.fail = true;
+                                app.message = response.data.fail;
+                                app.loading = false;
+                            } else {
+                                app.items = response.data.items;
+                                app.cartTotal = response.data.cartTotal;
+                                app.loading = false;
+                            }
+                        })
+                    }, 2000);
+                }
+            },
+
+            created: function () {
+                this.displayItems();
             }
         });
     };
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -8,6 +8,57 @@
         <div class="text-center">
             <img v-show="loading" src="/images/loading.gif">
         </div>
+
+        <section class="items" v-if="loading == false">
+            <div class="row">
+                <div class="small-12">
+                    <h2 v-if="fail" v-text="message"></h2>
+                    <div v-else>
+                        <h2>Your Cart</h2>
+
+                        <table class="hover instriped">
+                            <thead class="text-left">
+                                <tr>
+                                    <th>#</th><th>Product Name</th>
+                                    <th>($) Unit Price</th> <th>Qty</th><th>Total</th> <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="item in items">
+                                    <td class="medium text-center">
+                                        <a :href="'/product/' + item.id">
+                                            <img :src="'/' + item.image" height="60px" width="60px" alt="item.name">
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <h5>
+                                            <a :href="'/product/' + item.id">
+                                                @{{ item.name }}
+                                            </a>
+                                        </h5>
+                                        Status:
+                                        <span v-if="item.stock > 1" style="color:#00AA00;">
+                                            In Stock
+                                        </span>
+                                        <span v-else style="color:#ff0000;">
+                                            Out Of Stock
+                                        </span>
+                                    </td>
+                                    <td>@{{ item.price }}</td>
+                                    <td>@{{ item.quantity }}</td>
+                                    <td>@{{ item.total }}</td>
+                                    <td class="text-center">
+                                        <button>
+                                            <i class="fa fa-times" aria-hidden="true"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
         
     </div>
 @stop
